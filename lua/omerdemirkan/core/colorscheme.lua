caafb86a0a31a829c739836f6237c7b540662b60
omerdemirkan <omerfarukpiano@gmail.com>
2022-12-22 11:40:41 -0800

    change color scheme for higher bg contrast

--- lua/omerdemirkan/core/colorscheme.lua
+++ lua/omerdemirkan/core/colorscheme.lua
@@ -1,29 +1,26 @@
-
 local tokyonight_status, tokyonight = pcall(require, "tokyonight")
 
 if not tokyonight_status then
-  return
+	return
 end
 
-
 tokyonight.setup({
-  -- transparent = true,
-  on_colors = function(colors)
-    colors["bg_float"] = "#12121f"
-    colors["bg_statusline"] = "#12121f"
-    colors["bg_sidebar"] = "#12121f"
-    colors["bg_popup"] = "#12121f"
-    colors["bg_dark"] = "#12121f"
+	-- transparent = true,
+	on_colors = function(colors)
+		colors["bg_float"] = "#1a1a2b"
+		colors["bg_statusline"] = "#1a1a2b"
+		colors["bg_sidebar"] = "#1a1a2b"
+		colors["bg_popup"] = "#1a1a2b"
+		colors["bg_dark"] = "#1a1a2b"
 
-    colors["bg"] = "#06060a"
+		colors["bg"] = "#06060a"
 
-    colors["bg_highlight"] = "#181c29"
-  end
+		colors["bg_highlight"] = "#222736"
+	end,
 })
 
 local status, _ = pcall(vim.cmd, "colorscheme tokyonight-night")
 if not status then
-  print("Color scheme not found!")
-  return
+	print("Color scheme not found!")
+	return
 end
-
